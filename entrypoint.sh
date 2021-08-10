21c87970b75f385ea1cd78fb615bd7c80e07a17b
Andre Sencioles <asenci@gmail.com>
2021-08-10 16:09:13 +1200

    Output plain text report along with github errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 set -o errexit
 
+REPORT_FILE=$(mktemp -u).console
+
 /usr/local/bin/psalm \
   --output-format='github' \
-  --threads "$(nproc)" \
-  "${@}"
+  --report="${REPORT_FILE}" \
+  --report-show-info='false' \
+  --threads="$(nproc)" \
+  "${@}" || EXIT_CODE=${?}
+
+test -f "${REPORT_FILE}" && cat "${REPORT_FILE}"
+
+exit "${EXIT_CODE}"
